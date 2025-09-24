bf9d9f6318aed0dfefde87a5e9baf4e5422cb798
maplefukku <channefukku2940@gmail.com>
2025-09-24 20:04:04 +0900

    test(ci): developmentブランチでのdeploy-branch.yml動作テスト

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
+// CI/CDテスト: developmentブランチでのdeploy-branch.yml動作確認
 import 'firebase_options.dart';
 import 'app.dart';
 import 'core/errors/global_error_handler.dart';
